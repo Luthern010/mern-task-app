0dd386e977530839db573ae001cef9e03a5ec31d
IBRAHIM Ajayi <ajhimd@gmail.com>
2023-10-04 18:09:12 +0100

    server update

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+
+const dotenv = require("dotenv").config()
+const express = require("express")
+const connectDB = require("./config/connectDB")
+const mongoose = require("mongoose")
+const Task = require("./models/taskModel")
+const taskRoutes  = require("./routes/taskRoute")
+const app = express();
+connectDB()
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+app.use(taskRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Home page")
+})
+
+
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+        .connect(process.env.MONGO_URI)
+        .then(() => {
+          app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+            });
+        }) 
+        .catch((err) => console.log(err))
+
+
