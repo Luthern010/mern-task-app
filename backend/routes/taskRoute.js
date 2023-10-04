7c569af0d664d0994256d1435fa7d0963ee7c565
IBRAHIM Ajayi <ajhimd@gmail.com>
2023-10-04 22:07:31 +0100

    Getting a hang of this

--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -3,11 +3,12 @@ const Task = require("../models/taskModel");
 const { createTask, getTasks, getTask, deleteTask, updateTask } = require("../controllers/taskController");
 const router = express.Router();
 
-
-router.post("/api/tasks", createTask); 
-router.get("/api/tasks", getTasks);
-router.get("/api/tasks/:id", getTask);
-router.delete("/api/tasks/:id", deleteTask);
-router.put("/api/tasks/:id", updateTask)
+router.route("/").get(getTasks).post(createTask)
+router.route("/:id").get(getTask).delete(deleteTask).put(updateTask)
+// router.post("/", createTask); 
+// router.get("/", getTasks);
+// router.get("/:id", getTask);
+// router.delete("/:id", deleteTask);
+// router.put("/:id", updateTask);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB()
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
-app.use(taskRoutes);
+app.use("/api/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
   res.send("Home page")
